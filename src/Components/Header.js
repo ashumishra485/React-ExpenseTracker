02147a2f191f194b56d495e8c5a28a03e7c7222c
Ashutosh Mishra <55360225+ashumishra485@users.noreply.github.com>
2021-12-21 17:33:00 +0000

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import StackedLineChartIcon from '@mui/icons-material/StackedLineChart';
+
+export const Header = () => {
+    return (
+        <div>
+            <h1>Expense Tracker   <StackedLineChartIcon/></h1>
+       </div>
+    )
+}
+
+
+
+
